2ae0fd7bef18cac708df79494ac6f51f71c81f67
yafei.wang <yafei.wang@outlook.com>
2016-04-26 16:55:05 +1200

    dev

--- snakes2.js
+++ snakes2.js
@@ -12,32 +12,43 @@ window.onkeyup = function (e) {
 	var key = e.keyCode ? e.keyCode : e.which;
 	var dir;
 	switch (key) {
-	case 13: // enter key
-		if (snakeGame.started == false) {
-			snakeGame.start();
-		} else {
-			snakeGame.stop();
-		}
-		break;
-	case 37: // 
-		snakeGame.snake.turn("left");
-		break;
-	case 38:
-		snakeGame.snake.turn("up");
-		break;
-	case 39:
-		snakeGame.snake.turn("right");
-		break;
-	case 40:
-		snakeGame.snake.turn("down");
-		break;
-	default:
-		//alert("invalid input");
+        case 13: // enter key
+            switch (snakeGame.status) {
+                case "not started":
+                    snakeGame.start();
+                    break;
+
+                case "stopped":
+                    snakeGame.resume();
+                    break;
+
+                case "started":
+                    snakeGame.stop();
+                    break;
+
+                default:
+            }
+            break;
+        case 37: // 
+            snakeGame.snake.turn("left");
+            break;
+        case 38:
+            snakeGame.snake.turn("up");
+            break;
+        case 39:
+            snakeGame.snake.turn("right");
+            break;
+        case 40:
+            snakeGame.snake.turn("down");
+            break;
+        default:
+            //alert("invalid input");
 	}
 };
 
 class SnakeGame {
 	constructor() {
+        this.status = "not started"
 		this.started = false;
 		this.width = 64;
 		this.height = 48;
@@ -54,15 +65,25 @@ class SnakeGame {
 		this.canvasPainter = new SquarePainter(this.context, this.pixelsPerSquare, this.canvasColor);
 		this.foodPainter = new SquarePainter(this.context, this.pixelsPerSquare, this.foodColor);
 		this.snake = new Snake(this.gameCanvas.center(), this.snakePainter, this.canvasPainter);
-		
-		// create the food generator
-		this.foodFactory = new FoodFactory(this.gameCanvas);
-		this.food = this.foodFactory.generate();
 	};
 	
 	everyInterval() {
-		this.snake.move();
-		if (this.snake.head().x !== this.food.x || this.snake.head().y !== this.food.y) {
+		// move the snake
+        if (this.snake.move() == false) {
+            this.gameOver();
+            return;
+        }
+        
+        // --- game over check ---
+        // run out?
+        var snakeHead = this.snake.head();
+        if (this.gameCanvas.outOfCanvas(snakeHead)) {
+            this.gameOver();
+			return;
+        } 
+        
+        // get the food?
+		if (!this.snake.head().equals(this.food.point)) {
 			this.canvasPainter.paint(this.snake.body.pop());	
 		} else {
 			this.generateFood();
@@ -72,13 +93,12 @@ class SnakeGame {
 	generateFood() {
 		while (true) {
 			var gotIt = true;
-			var food = this.foodFactory.generate();
-			if (this.snake.partOfBody(food)) {
+			var point = this.gameCanvas.getARandomPointOnCanvas();
+			if (this.snake.partOfBody(point)) {
 				gotIt = false;
 			}
 			if (gotIt === true) {
-				this.food = food;
-				this.foodPainter.paint(food);
+				this.food = new Food(point, this.foodPainter, this.canvasPainter);
 				break;
 			}
 		}
@@ -86,26 +106,29 @@ class SnakeGame {
 	
 	start() {
 		this.generateFood();
-		this.snakeInterval = setInterval(everyInterval, 200);
-		this.started = true;
+		this.resume();
 	};
 	
 	stop() {
 		clearInterval(this.snakeInterval);
-		this.started = false;
+		this.status = "stopped";
 	};
-	
-	restart() {
-		this.stop();
-		while (this.body.length > 0) {
-			this.paintToCanvas(this.body.pop());
-		}
-		this.snake.init();
-	};
+    
+    resume() {
+        this.snakeInterval = setInterval(everyInterval, 100);
+		this.status = "started";
+    };
+    
+    clear() {
+        clearInterval(this.snakeInterval);
+        this.snake.clear();
+        this.food.clear();
+        this.status = "not started";
+    };
 	
 	gameOver() {
-		this.restart();
 		alert("Game Over!");
+        this.clear();
 	};
 }
 
@@ -124,7 +147,18 @@ class GameCanvas {
 	
 	center() {
 		return new Point(Math.round(this.width / 2) - 1, Math.round(this.height / 2) - 1);
-	}
+	};
+    
+    outOfCanvas(pos) {
+        return (pos.x < 0 || pos.x > this.width - 1 || pos.y < 0 || pos.y > this.height - 1);
+    };
+    
+    getARandomPointOnCanvas() {
+        var x = Math.round(Math.random() * this.width);
+        var y = Math.round(Math.random() * this.height);
+        
+        return new Point(x, y);
+    };
 	
 	draw() {
         for (var i = 1; i < this.height; i++) {
@@ -146,7 +180,7 @@ class GameCanvas {
 class Snake {
 	constructor (canvasCenter, snakePainter, canvasPainter) {
 		this.snakePainter = snakePainter;
-		this.ffoodcanvasPainter = canvasPainter;
+		this.canvasPainter = canvasPainter;
 		this.canvasCenter = canvasCenter;
 		this.init();
     };
@@ -165,35 +199,52 @@ class Snake {
 	};
 	
 	partOfBody(pos) {
-		return this.body.indexOf(pos) > -1;
-	}
+        for (var i=0; i<this.body.length; i++) {
+            if (this.body[i].equals(pos)) {
+                return true;
+            }   
+        }    
+        return false;
+	};
+    
+    clear() {
+        while (this.body.length > 0) {
+			this.canvasPainter.paint(this.body.pop());
+		}
+        
+		this.init();
+    };
  	
 	move () {
 		var currentHead = this.body[0];
         var newHead;
         switch (this.direction) {
-        case "up":
-            newHead = currentHead.itsAbovePoint();
-            break;
+            case "up":
+                newHead = currentHead.itsAbovePoint();
+                break;
 
-        case "down":
-            newHead = currentHead.itsBelowPoint();
-            break;
+            case "down":
+                newHead = currentHead.itsBelowPoint();
+                break;
 
-        case "left":
-            newHead = currentHead.itsLeftPoint();
-            break;
+            case "left":
+                newHead = currentHead.itsLeftPoint();
+                break;
 
-        case "right":
-            newHead = currentHead.itsRightPoint();
-            break;
+            case "right":
+                newHead = currentHead.itsRightPoint();
+                break;
 
-        default:
+            default:
         }
 		
-		this.body.unshift(newHead);
-		this.snakePainter.paint(newHead);
-	
+        if (this.partOfBody(newHead)) {
+            return false;
+        } else {
+            this.body.unshift(newHead);
+		    this.snakePainter.paint(newHead);
+            return true;
+        }
     };
 	
 	turn (direction) {
@@ -201,20 +252,29 @@ class Snake {
 		   || (this.direction === "left" || this.direction === "right") && (direction === "up" || direction === "down")) {
 			this.direction = direction;
 		}
-	}
+	};
 }
 
-class FoodFactory {
-	constructor (gameCanvas) {
-		this.gameCanvas = gameCanvas;
-	};
-	
-	generate() {
-        var x = Math.round(Math.random() * this.gameCanvas.width);
-        var y = Math.round(Math.random() * this.gameCanvas.height);
-            
-      	return new Point(x, y);
-	};
+class Food {
+    constructor(point, foodPainter, canvasPainter) {
+        this.point = point;
+        this.foodPainter = foodPainter;
+        this.canvasPainter = canvasPainter;
+        
+        this.foodPainter.paint(this.point);
+    };
+    
+    clear() {
+        this.canvasPainter.paint(this.point);
+    };
+}
+
+class Monster {
+    constructor() {
+        
+    };
+    
+    
 }
 
 class Point {
@@ -222,6 +282,10 @@ class Point {
 		this.x = x;
 		this.y = y;
 	};
+    
+    equals(anotherPoint) {
+        return this.x === anotherPoint.x || this.y === anotherPoint.y
+    };
 	
 	itsLeftPoint() {
 		return new Point(this.x - 1, this.y);
